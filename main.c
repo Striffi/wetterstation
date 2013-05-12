22033c85d76c4007e5e71b0cdecf46fdfe975e38
sakati <johndoe3@gmx.at>
2013-05-12 19:53:31 +0200

    adding config-files, working on mainfile

--- main.c
+++ main.c
@@ -1,54 +1,79 @@
 #include <stdio.h>
 #include <errno.h>
 #include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 #include "common.h"
-/*#include "critvalues.h"*/
+#include "mode.h"
 
 #define TRUE 0;
 #define FALSE 1;
 
-/*int isCritical(double value, char mode)
+long PRESVALUES[8];
+/*0 pres_verylow
+ 1 pres_low_min
+ 2 pres_low_max
+ 3 pres_normal_min
+ 4 pres_normal_max
+ 5 pres_high_min
+ 6 pres_high_max
+ 7 pres_veryhigh*/
+
+
+int isCritical(double value, char mode)
 {
 	if (mode == 't')
 	{
-		if (value < critTempLow || value > critTempHigh)
+		/*if (value < critTempLow || value > critTempHigh)
 		{
 			return TRUE;
 		}
 		else
 		{
 			return FALSE;
-		}
+		}*/
 	}
 	else if (mode == 'h')
 	{
-		if (value < critHumLow || value > critHumHigh)
+		/*if (value < critHumLow || value > critHumHigh)
 		{
 			return TRUE;
 		}
 		else
 		{
 			return FALSE;
-		}
+		}*/
 	}
 	else if (mode == 'p')
 	{
-		if (value < critPresLow || value > critPresHigh)
+		if (value < PRESVALUES[0])
 		{
-			return TRUE;
+			return PRES_VERYLOW;
 		}
-		else
+		else if(value > PRESVALUES[1] && value < PRESVALUES[2])
 		{
-			return FALSE;
+			return PRES_LOW;
+		}
+		else if(value > PRESVALUES[3] && value < PRESVALUES[4])
+		{
+			return PRES_NORMAL;
+		}
+		else if (value > PRESVALUES[5] && value < PRESVALUES[6])
+		{
+			return PRES_HIGH;
+		} else
+		{
+			return PRES_VERYHIGH;
 		}
 	}
 	else
 	{
 		printf("Mode does not exist");
-		return 1;
+		return -1;
 	}
 
-}*/
+}
 
 void doSleep(int sec, int nsec)
 {
@@ -69,6 +94,7 @@ void doSleep(int sec, int nsec)
 	}
 }
 
+/*TODO*/
 int writeToXML(double temp, double hum, double pres, struct tm *timeofday)
 {
 	temp=temp;
@@ -87,6 +113,95 @@ struct tm *getTime(struct tm *timeofday)
 	return timeofday;
 }
 
+void readConfig(char *file, int flag, long *sec, long *nsec)
+{
+	FILE *config;
+	char buf[80];
+	char *help;
+	int i=0;
+	char *endptrstrtol = NULL;
+	
+	if((config = fopen(file, "r")) != NULL)
+	{
+		while (fgets(buf,80,config) != NULL)
+		{
+			if ((help = strchr(buf, '=')) != NULL)
+			{
+				if (flag == 0)
+				{
+					errno = 0;
+					PRESVALUES[i] = strtol(help+1, &endptrstrtol, 10);
+					if ((errno == ERANGE && (PRESVALUES[i] == LONG_MAX || PRESVALUES[i] == LONG_MIN)) || (errno != 0 && PRESVALUES[i] == 0)) {
+						fprintf(stderr, "%s\n",strerror(errno));
+						exit(EXIT_FAILURE);
+					}
+
+					if (endptrstrtol == help+1) {
+						fprintf(stderr, "No digits were found\n");
+						exit(EXIT_FAILURE);
+					}
+				} 
+				else
+				{
+					switch(i)
+					{
+						errno = 0;
+						case 0:{ 
+							*sec = strtol(help+1, &endptrstrtol, 10);
+							if ((errno == ERANGE && (*sec == LONG_MAX || *sec == LONG_MIN)) || (errno != 0 && *sec == 0)) {
+								fprintf(stderr, "%s\n",strerror(errno));
+								exit(EXIT_FAILURE);
+							}
+							if (endptrstrtol == help+1) {
+								fprintf(stderr, "No digits were found\n");
+								exit(EXIT_FAILURE);
+							}
+							break;
+						}
+						case 1:{
+							*nsec = strtol(help+1, &endptrstrtol, 10);
+							if ((errno == ERANGE && (*nsec == LONG_MAX || *nsec == LONG_MIN)) || (errno != 0 && *nsec == 0)) {
+								fprintf(stderr, "%s\n",strerror(errno));
+								exit(EXIT_FAILURE);
+							}
+							if (endptrstrtol == help+1) {
+								fprintf(stderr, "No digits were found\n");
+								exit(EXIT_FAILURE);
+							}
+							break;
+						}
+						default:{
+							fprintf(stderr, "error reading timer.config");
+							break;
+						}
+					}
+				}		
+				
+				fclose(config);
+			}
+			else
+			{
+				/*error searching "=" in config-file*/
+				fclose(config);
+				exit(EXIT_FAILURE);
+			}
+			i++;
+		}
+		if (errno != 0)
+		{
+			/*error while fgets*/
+			fclose(config);
+			exit(EXIT_FAILURE);
+		}
+	} 
+	else
+	{
+		/*error opening config-file*/
+		exit(EXIT_FAILURE);
+	}
+	
+}
+
 int main()
 {
 	double temp = 0.0;
@@ -94,9 +209,13 @@ int main()
 	double pres = 0.0;
 	struct temphum th;
 	struct tm timeofday;
-	int sec = 20; /*reading from properties file TODO*/
-	int nsec = 0;
+	long sec = 0;
+	long nsec = 0;
 	int helpI = 0;
+	
+	readConfig("critvales.config", 0, NULL, NULL);
+	
+	readConfig("timer.config", 1, &sec, &nsec);
 	
 	while (1)
 	{
@@ -120,18 +239,15 @@ int main()
 			helpI = 0;
 		}
 		
-		/* writeLED(isCritical(hum, 't')0); */
-		writeLED(0);
+		writeLED(isCritical(temp, 't'));
 		writeLCD(temp, 't', &timeofday);
 		doSleep(sec, nsec);
 	
-		/* writeLED(isCritical(hum, 't')0); */
-		writeLED(0);
+		writeLED(isCritical(hum, 'h'));
 		writeLCD(hum, 'h', &timeofday);
 		doSleep(sec, nsec);
 	
-		/* writeLED(isCritical(hum, 't')0); */
-		writeLED(0);
+		writeLED(isCritical(pres, 'p'));
 		writeLCD(pres, 'p', &timeofday);
 		doSleep(sec, nsec);
 	}
---
+++ mode.h
@@ -0,0 +1,6 @@
+
+#define PRES_NORMAL 0;
+#define PRES_HIGH 1;
+#define PRES_VERYHIGH 2;
+#define PRES_LOW 3;
+#define PRES_VERYLOW 4;
