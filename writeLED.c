45ee01be81ae8e868c573034dead68c6f8327aae
ion777 <pi@raspberrypi.(none)>
2013-06-12 21:43:17 +0000

    und nochmal writeLED.c verbessert

--- writeLED.c
+++ writeLED.c
@@ -68,15 +68,12 @@ switch (LED_mode) {
                 doSleep(0,125000000);
                 break;
 
-	default:
-
-                pinMode(3,OUTPUT);
-                pinMode(0,OUTPUT);
-                digitalWrite(0,1);
-		digitalWrite(3,1);
+	default: /*signaling ERROR*/
+                
+		pinMode(3,OUTPUT);
+                digitalWrite(3,1);
                 doSleep(0,750000000);
-                digitalWrite(0,0);
-		digitalWrite(3,0);
+                digitalWrite(3,0);
                 doSleep(0,250000000);
                 break;
 	}
@@ -86,28 +83,23 @@ return 0;
 
 extern void writeLED(uint8_t mode) {
 int x = -1;
+const char FNAME[] = "writeLED()";
 pthread_t thread;
 
 /* start thread only if mode has changed */
 if (mode != LED_mode) {
 	
 	if (mode != 0) pthread_cancel(thread); /* cancel old thread*/
-
 	pinMode(0,OUTPUT);
 	pinMode(2,OUTPUT);
 	digitalWrite(0,0);
 	digitalWrite(2,0);
 	
 	LED_mode = mode;
-	/*
-	if (wiringPiSetup() == -1)
-        	printf("Troubles with WiringPi ...\n");
-
-	x = piThreadCreate(LED_output);*/
 	
 	x = pthread_create(&thread, NULL, &LED_output, NULL);
 
-	if (x != 0) printf ("LED_output didn´t started\n");
+	if (x != 0) fprintf (stdout, "DEBUG: %s: LED_output didn´t started\n", FNAME);
 	}
 }
 
